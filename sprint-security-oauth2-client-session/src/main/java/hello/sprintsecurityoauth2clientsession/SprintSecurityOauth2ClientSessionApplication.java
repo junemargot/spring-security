f4299f7dd6b2d79ac7f4f27489b5e3e9a8a1e577
margotinjune <margotinjune@gmail.com>
2025-04-24 19:51:29 +0900

    Initial commit

---
+++ sprint-security-oauth2-client-session/src/main/java/hello/sprintsecurityoauth2clientsession/SprintSecurityOauth2ClientSessionApplication.java
@@ -0,0 +1,13 @@
+package hello.sprintsecurityoauth2clientsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintSecurityOauth2ClientSessionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SprintSecurityOauth2ClientSessionApplication.class, args);
+  }
+
+}
---
+++ sprint-security-oauth2-client-session/src/test/java/hello/sprintsecurityoauth2clientsession/SprintSecurityOauth2ClientSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.sprintsecurityoauth2clientsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintSecurityOauth2ClientSessionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
