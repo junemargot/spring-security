1075046bff2a92c10362abb8b5722b4288741a6e
margotinjune <margotinjune@gmail.com>
2025-05-02 19:06:11 +0900

    initial commit

---
+++ spring-security-oauth2-jwt/src/test/java/hello/oauth2jwt/SpringSecurityOauth2JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.oauth2jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2JwtApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
