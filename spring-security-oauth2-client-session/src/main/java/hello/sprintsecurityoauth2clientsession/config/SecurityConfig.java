fc188f9730a997c8667204df21c7fcabeceffdb0
margotinjune <margotinjune@gmail.com>
2025-05-02 16:37:30 +0900

    feat: OAuth2 인증 토큰 JDBC 저장 방식 설정 및 테이블 추가 - JdbcOAuth2AuthorizedClientService를 사용하여 OAuth2 토큰 정보를 DB에 저장하도록 설정 - 사용자 인증 시 access token 및 refresh token이 oauth2_authorized_client 테이블에 저장됨 - SecurityConfig  oauth2Login 구성에 clientRegistrationRepository 및 authorizedClientService 명시적 주입 - 추후 클러스터/세션 공유 대응 가능

--- spring-security-oauth2-client-session/src/main/java/hello/sprintsecurityoauth2clientsession/config/SecurityConfig.java
+++ spring-security-oauth2-client-session/src/main/java/hello/sprintsecurityoauth2clientsession/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package hello.sprintsecurityoauth2clientsession.config;
 
 import hello.sprintsecurityoauth2clientsession.oauth2.CustomClientRegistrationRepo;
+import hello.sprintsecurityoauth2clientsession.oauth2.CustomOAuth2AuthorizedClientService;
 import hello.sprintsecurityoauth2clientsession.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,10 +17,14 @@ public class SecurityConfig {
 
   private final CustomOAuth2UserService customOAuth2UserService;
   private final CustomClientRegistrationRepo customClientRegistrationRepo;
+  private final CustomOAuth2AuthorizedClientService customOAuth2AuthorizedClientService;
+  private final JdbcTemplate jdbcTemplate;
 
-  public SecurityConfig(CustomOAuth2UserService customOAuth2UserService, CustomClientRegistrationRepo customClientRegistrationRepo) {
+  public SecurityConfig(CustomOAuth2UserService customOAuth2UserService, CustomClientRegistrationRepo customClientRegistrationRepo, CustomOAuth2AuthorizedClientService customOAuth2AuthorizedClientService, JdbcTemplate jdbcTemplate) {
     this.customOAuth2UserService = customOAuth2UserService;
     this.customClientRegistrationRepo = customClientRegistrationRepo;
+    this.customOAuth2AuthorizedClientService = customOAuth2AuthorizedClientService;
+    this.jdbcTemplate = jdbcTemplate;
   }
 
   @Bean
@@ -41,6 +47,7 @@ public class SecurityConfig {
             .oauth2Login((oauth2) -> oauth2
                     .loginPage("/login")
                     .clientRegistrationRepository(customClientRegistrationRepo.getClientRegistration())
+                    .authorizedClientService(customOAuth2AuthorizedClientService.oAuth2AuthorizedClientService(jdbcTemplate, customClientRegistrationRepo.getClientRegistration()))
                     .userInfoEndpoint((userInfoEndpointConfig) ->
                             userInfoEndpointConfig.userService(customOAuth2UserService))
                     );
